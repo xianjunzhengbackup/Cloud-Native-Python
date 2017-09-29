7bd1d46540f7bcfa2855d5a0a7079663ca6ad374
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2017-09-29 15:01:58 +1000

    add POST method

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask ,jsonify
+from flask import Flask,jsonify,request,abort
 import sqlite3
      
 app = Flask(__name__)      
 
+"""
+curl -i -H "Content-Type: application/json" http://localhost:5000/api/v1/users
+
+The above command could send GET command to server
+"""
 @app.route('/api/v1/users',methods=['GET'])
 def get_users():
 	return list_users()
@@ -23,6 +28,38 @@ def list_users():
 
 	conn.close()
 	return jsonify({'user_list':api_list})	
+
+"""
+curl -i -H "Content-Type: application/json"-X POST -d '{"username":"jun11","email":"jun11@email","password":"111","name":"jun111"}' http://localhost:5000/api/v1/users
+
+The above command could post data to the server
+"""
+@app.route('/api/v1/users',methods=['POST'])
+def create_user():
+	if not request.json or not 'username' in request.json\
+or not 'email' in request.json or not 'password' in request.json:
+		abort(400)
+	user={'username':request.json['username'],\
+'email':request.json['email'],\
+'name':request.json['name'],\
+'password':request.json['password']}
+	return jsonify({'status':add_user(user)}),201
+
+def add_user(new_user):
+	conn=sqlite3.connect("mydb.db")
+	print("Open database successfully");
+	api_list=[]
+	cursor=conn.cursor()
+	cursor.execute("SELECT * from users where username=? or email=?",(new_user['username'],new_user['email']))
+	data=cursor.fetchall()
+	if len(data) != 0:
+		abort(409)
+	else:
+		cursor.execute("insert into users (username,email,password,name) values(?,?,?,?)",(new_user['username'],new_user['email'],new_user['password'],new_user['name']))
+		conn.commit()
+		return "Success"
+	conn.close()
+	return jsonify(a_dict)
 
 if __name__ == "__main__":      
 	app.run(host='0.0.0.0', port=5000, debug=True) 
