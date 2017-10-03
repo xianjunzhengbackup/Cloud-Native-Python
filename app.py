968239627678a8a1d1f51e12ef5d088c6c9aad20
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2017-10-03 15:50:48 +1100

    add put request

--- app.py
+++ app.py
@@ -110,5 +110,38 @@ def del_user(del_user):
 		return "Success"
 	conn.close()
 
+"""
+curl -i -H "Content-Type: application/json" -X put -d '{"password":"2222"}' http://localhost:5000/api/v1/users/4
+run above command to modify password for userid==4
+"""
+@app.route('/api/v1/users/<int:user_id>',methods=['PUT'])
+def update_users(user_id):
+	user={}
+	if not request.json:
+		abort(400)
+	user['id']=user_id
+	key_list=request.json.keys()
+	for i in key_list:
+		user[i]=request.json[i]
+	print(user)
+	return jsonify({'status':update_user(user)}),200
+
+def update_user(user):
+	conn=sqlite3.connect('mydb.db')
+	print("Opened database successfully")
+	cursor=conn.cursor()
+	cursor.execute("SELECT * from users where id=? ",(user['id'],))
+	data=cursor.fetchall()
+	if len(data) == 0:
+		abort(404)
+	else:
+		key_list=user.keys()
+		for i in key_list:
+			if i != "id":
+				print(user,i)
+				cursor.execute("""UPDATE users SET {0} = ? WHERE id=?""".format(i),(user[i],user['id']))
+				conn.commit()
+		return("Successfully")
+
 if __name__ == "__main__":      
 	app.run(host='0.0.0.0', port=5000, debug=True) 
