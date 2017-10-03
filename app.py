7f19f5b7da0834de86642ac8d9c507990632bc0c
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2017-10-03 12:51:29 +1100

    add delete function

--- app.py
+++ app.py
@@ -84,5 +84,31 @@ def add_user(new_user):
 	conn.close()
 	return jsonify(a_dict)
 
+"""
+curl -i -H "Content-Type: application/json" -X delete -d '{"username":"jun1"}' http://localhost:5000/api/v1/users
+Use the above command to delete user
+"""
+@app.route('/api/v1/users',methods=['DELETE'])
+def delete_user():
+	if not request.json or not 'username' in request.json:
+		abort(400)
+	user=request.json['username']
+	return jsonify({'status':del_user(user)}),200
+
+def del_user(del_user):
+	conn=sqlite3.connect('mydb.db')
+	print("Opened database successfully")
+	cursor=conn.cursor()
+	cursor.execute("SELECT * from users where username=?",(del_user,))
+	data=cursor.fetchall()
+	print("Data",data)
+	if len(data) == 0:
+		abort(404)
+	else:
+		cursor.execute("delete from users where username==?",(del_user,))
+		conn.commit()
+		return "Success"
+	conn.close()
+
 if __name__ == "__main__":      
 	app.run(host='0.0.0.0', port=5000, debug=True) 
